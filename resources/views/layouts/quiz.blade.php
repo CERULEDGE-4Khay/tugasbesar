c0a887234501e34b39d8702281494bb598d1d96a
CERULEDGE-4Khay <khairankren22@gmail.com>
2025-06-16 12:01:59 +0700

    menambahkan template untuk landing page dan dashboard

---
+++ resources/views/layouts/quiz.blade.php
@@ -0,0 +1,3 @@
+
+
+<h1>QUIZ SESSION</h1>
--- routes/web.php
+++ routes/web.php
@@ -18,14 +18,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return view('login');
+});
 
-// Route::get('/login', function () {
-//     return view('login');
-// });
+Route::get('/register', function () {
+    return view('register');
+});
+Route::get('quiz', function () {
+    return view('layouts.quiz');
+})->name('quiz');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
 
-// Route::get('/register', function () {
-//     return view('register');
-// });
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
